b31647b36bd1d77ed71fd88c49e86fbb4299c6f5
Zhang Weijian <zhangweijian97@outlook.com>
2019-07-14 23:02:06 +0800

    finish my cost Function

--- week3LogisticRegression/machine-learning-ex2/ex2/costFunction.m
+++ week3LogisticRegression/machine-learning-ex2/ex2/costFunction.m
@@ -20,12 +20,13 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+m = length(y);
 
+h = sigmoid(X * theta);
 
+J = 1/m * (-y' * log(h) - (1 - y)' * log(1-h)) ;
 
-
-
-
+grad = 1/m * X' * (sigmoid(X * theta) - y);
 
 % =============================================================
 
